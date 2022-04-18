f2ea0f3fbde7194615c57fb9a3491c67f8ba1515
Brayan Jenzy La Cruz Carreño <blacruzc@emeal.nttdata.com>
2022-04-18 17:31:26 -0500

    gitignore

--- src/main/java/com/bootcamp/corporateclients/CorporateclientsApplication.java
+++ src/main/java/com/bootcamp/corporateclients/CorporateclientsApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CorporateclientsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CorporateclientsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    System.setProperty("jdk.tls.client.protocols", "TLSv1.2");
+    SpringApplication.run(CorporateclientsApplication.class, args);
+  }
 
 }
